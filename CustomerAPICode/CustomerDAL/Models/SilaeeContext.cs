5d5e28227c05f75934f683ebe6cc03ea8e66e147
faisal hameed <faysal_hameed@hotmail.com>
2020-07-26 13:07:08 +0500

    connection string changed

--- CustomerAPICode/CustomerDAL/Models/SilaeeContext.cs
+++ CustomerAPICode/CustomerDAL/Models/SilaeeContext.cs
@@ -22,7 +22,7 @@ namespace CustomerDAL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=localhost;Database=CustomerDB;user id=sa;password=123;");
+                optionsBuilder.UseSqlServer("Server=sql5052.site4now.net; Database=CustomerDB;user id=DB_A6503A_CustomerDB_admin;password=K@2Te@m123;");
             }
         }
 
