6ef9d354c40c8cbd79e86e712b59cb86b6b25686
faisal hameed <faysal_hameed@hotmail.com>
2020-09-06 19:56:24 +0500

    removed tailor reference

--- CustomerAPICode/CustomerAPI/Controllers/WeatherForecastController.cs
+++ CustomerAPICode/CustomerAPI/Controllers/WeatherForecastController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using CustomerDAL.Tailor;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
