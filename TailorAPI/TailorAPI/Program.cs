3b8746634ab07f1d2255c5945235a0295e336271
faisal hameed <faysal_hameed@hotmail.com>
2020-09-12 13:16:31 +0500

    hi

--- TailorAPI/TailorAPI/Program.cs
+++ TailorAPI/TailorAPI/Program.cs
@@ -16,6 +16,8 @@ namespace TailorAPI
             CreateHostBuilder(args).Build().Run();
         }
 
+
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
